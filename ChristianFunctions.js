52e965072ec2454d969c3ccff05856bf1cc618a5
Lucia Johnson <76014389+lujoh@users.noreply.github.com>
2020-12-15 19:28:16 -0500

    Adding the Javascript file for the website
    The first part contains functions to show the contact form when needed, the second part has the functions that operate a slideshow.

---
+++ ChristianFunctions.js
@@ -0,0 +1,39 @@
+/*Functions to show an item. 
+Used to hide and show Kontaktform by button click*/
+function showObject(IDName) {
+    if (document.getElementById(IDName)) {
+        document.getElementById(IDName).style.display = "block";
+    }
+}
+/*Function shows the Kontaktform and chenged the Betreff to the name of the button*/
+function showKontaktform(button) {
+    var betreffName = button.innerHTML.trim();
+    
+    showObject("hiddenKontakt");
+    document.getElementById("Betreff").value = betreffName;
+}
+
+
+/*Functions related to the slideshow*/
+var slideImages;
+var index = 0;
+var active = "slideshowFigure activeFigure";
+var inactive = "slideshowFigure";
+
+function changeClass(newClass, i) {
+    slideImages[i].className = newClass;
+}
+function cycleIndex() {
+    if (index >= slideImages.length) {
+        index = 0;
+    } else if (index < 0) {
+        index = slideImages.length - 1;
+    }
+}
+function switchImage(x) {
+    slideImages = document.getElementsByClassName("slideshowFigure");
+    changeClass(inactive, index);
+    index += x;
+    cycleIndex();
+    changeClass(active, index);
+}
