ff6b99a7e721039b98f45a9288ff800ae9557f7d
Lucia Johnson <76014389+lujoh@users.noreply.github.com>
2020-12-15 19:29:36 -0500

    Add files via upload
    Adding the Indienreise page. The bottom of the page contains the slideshow.

---
+++ Indienreise.php
@@ -0,0 +1,232 @@
+<?php
+/*Name der Seite*/
+$pageName = "Reiseseminare in Indien";
+/*Information fuer den meta tag*/
+$keywords_array = ["Swami Abhishiktananda", "Indienreise", "Indien", "Christian Hackbarth-Johnson", "Henri le Saux", "Spiritualität", "Spirituell", "Ganges", "Shakti", "Pilger"];
+$keywords_text = implode(", ", $keywords_array);
+$description = "Seminare in Indien zu dem Thema hinduistisch-christlicher Dialog nach Swami Abhishiktananda/Henri Le Saux.";
+?>
+
+<?php
+/*Hier wird der HTML opener und die Kopfzeile der Seite eingefuegt*/
+require_once 'Header.php';
+require_once 'Navigation.php';
+?>
+
+<!--Hier faengt der Inhalt an-->
+<div class="mainContent">
+    <div id="IndienreiseIntro">
+        
+        <h3 class="content">
+            Spirituelle Seminare in Indien auf den Spuren von Swami Abhishiktananda
+        </h3>
+        <img src="reiseIndien.jpg" class="imgRight" alt="Foto von der Quelle des Ganges. Fluss kommt aus dem Gletscher.">
+        
+        <p class="content">
+            Swami Abhishiktananda war ein französischer Benediktiner, der 1948 als Missionar nach Indien gegangen ist mit der Idee, die spirituelle Erfahrung Indiens in das Christentum zu integrieren. Er ist einer der großen Pioniere des interreligiösen Dialogs. Über ihn habe ich meine Doktorarbeit geschrieben mit dem Titel „Interreligiöse Existenz. Spirituelle Erfahrung und Identität bei Henri Le Saux (O.S.B.)/Swami Abhishiktananda (1910-1973). Seine Erfahrungen am Schnittpunkt zweier Religionen sind m.E. wegweisend für eine Spiritualität der Zukunft.
+        </p>
+        <p class="content">
+            Die Seminare in Indien, die ich anbiete, führen über sein Leben ein in den Hinduismus und den hinduistisch-christlichen Dialog. Wir nehmen teil am spirituellen Leben in den Ashrams, meditieren, üben Yoga, hören Vorträge (die ich, wenn nötig, dolmetsche) und tauschen uns aus. Indien ist immer eine tiefe und auf allen Ebenen zutiefst anrührende Erfahrung.
+        </p>
+        <p class="ctabutton">
+            <a href="aktuelleIndienreise.php">
+                Aktuelle Indienreise
+            </a>
+        </p>
+    </div><!--Closing tag IndianreiseIntro-->
+    <div id="Reisenbeschreibung">
+        <h3 class="content">
+            Reise 1: Südindien
+        </h3>
+        <p class="content">
+            <strong>Orte:</strong> Shantivanam Ashram, Sri Gnanananda Thapovanam, Sri Ramanasramam, Sri Aurobindo Ashram, Auroville ggf. Kurzaufenthalte in Kumbakonam, Chidambaram, Mamallapuram, gelegentlich auch Sameeksha Ashram (Kerala), Rameshwaram, Krishnamurti Study Centre in Chennai, Kanyakumari
+        </p>
+        <p class="content">
+            Der französische Priester Henri Le Saux kam 1948 nach Indien, um ein indisch-christliches Mönchtum grundzulegen. Er gründete zusammen mit einem anderen französischen Priester den Shantivanam Ashram, der später unter Bede Griffiths zu einem christlich-interreligiösen spirituellen Zentrum mit weltweiter Ausstrahlung wurde. Le Saux traf schon 1949 Sri Ramana Maharshi und erkannte die Tiefe der Herausforderung der indischen spirituellen Erfahrung. Nach dessen Tod kam er immer wieder an den heiligen Berg Arunachala, um wie Ramana in den Höhlen des Berges zu meditieren. Er suchte nach einem inneren Durchbruch, der ihm die tiefen Konflikte zwischen seiner christlichen Identität und der mystischen Erfahrung Indiens löste. Dabei traf er auf Sri Gnanananda, der für ihn zum Guru wurde. Später ging Le Saux nach Nordindien und lebte als Einsiedler im Himalaya. Seine Schriften sind eine bleibende Inspiration für spirituell suchende Menschen.(Durchgeführt in mehreren Varianten im Dez-Jan 2005-06; Dez 2007, Nov/Dez 2010 anlässlich des 100. Geburtstags von Henri Le Saux, Jan 2012; im Aug/Sept 2012 sowie Feb-März 2013, Nov-Dez 2013, Aug 2014, Sept 2015, Feb 2016 -zum Supramental Manifestation Day im Sri Aurobindo Ashram in Pondicherry).
+        </p>
+        <h3 class="content">
+            Reise 2: Nordindien
+        </h3>
+        <p class="content">
+            <strong>Orte:</strong> Sivananda Ashram in Rishikesh, Anandamayi Ashram in Haridwar, Christian Retreat and Study Center in Rajpur, HOPE-Projekt (Schule für Kinder im Slum) in Dehradun, Mussoorie  bzw. Almora, Advaita Ashram, Corbett National Park, Sri Aurobindo Ashram Dfelhi Branch
+        </p>
+        <p class="content">
+            Henri Le Saux/Swami Abhishiktananda (1910-1973) lebte viele Jahre als christlicher Sannyasi im Himalaya. Rishikesh ist das Tor zum Himalaya, der Ort, an dem der Ganges aus den Bergen in die Ebene kommt. Es ist das Tor zu den großen Pilgerwegen, die an die Quellen des Ganges führen. In Rishikesh leben Tausende von Mönchen und viele wichtige Ashrams befinden sich dort. Abhishiktananda weilte meist im Shivananda Ashram. An diesem Ort wurde er gepflegt, als er im Juli 1973 den Herzanfall erlitt, der für ihn zugleich eine tiefe spirituelle Erfahrung war, das „Finden des Grals“. (Durchgeführt im März 2005). Eine andere Reise führt nach Almora, wo das erste treffen der Sog. Cuttat-Group stattfand, das Le Saux zu zwei seiner theologischen Hauptwerke (Saccidananda und Hindu-Christian Meeting Point in the Cave of the Heart) inspirierte. Danach geht es zum Advaita Ashram der Ramakrishna Mission mit Blick auf die Himalaya-Kette (bei günstigem Wetter). In Delhi erwartet uns die zukunftsorienterte evolutionäre Spiritualität von Sri Aurobindo und der Mutter. (Durchgeführt im Sept. 2016).
+        </p>
+        <h3 class="content">
+            Reise 3: Auf den Spuren von Swami Abhishiktananda/Henri Le Saux zu den Quellen des Ganges (Uttarkashi, Gangotri/Indien)
+        </h3>
+        <p class="content">
+            Anfang der 1960er Jahre ließ sich Swami Abhishiktananda in Uttarkashi nieder, wo er sich eine Kutiya errichtete. Uttarkashi ist eine Pilgerstadt in den Bergen auf dem Weg nach Gangotri, zu den Quellen des Ganges. Die Pilgerreise zu den Gangesquellen steht für die Reise zurück zum Ursprung. Le Saux hat über seine eigenen Pilgerreisen in dem kleinen Büchlein “The Mountain of the Lord” geschrieben, das wir als Reiselektüre benutzen. Vor allem aber werden wir Texte aus den Upanishaden lesen und meditieren. Den Weg von Uttarkashi bis Gangotri (ca. 60 km), und von Gangotri zum Gletscher (ca 20 km) wollen wir zu Fuß gehen, und auf dem Weg in Pilgerunterkünften übernachten. Erstmals durchgeführt im Mai-Juni 2007 und Sept./Okt 2011).
+        </p>
+        <h3 class="content">
+            Reise 4: Shakti - der weibliche Aspekt der Gottheit
+        </h3>
+        <p class="content">
+            Diese Reise (erstmals durchgeführt Jan-Feb 2009) orientiert sich nicht an Abhishiktananda, sondern ist inspiriert durch die Erleuchtungserfahrung von Bede Griffiths, dem Nachfolger Abhishiktananda’s im Shantivanam Ashram, bei der er einen “Einbruch des Weiblichen” erlebte. Der weibliche Aspekt der Gottheit ist im Hinduismus überall präsent. In keiner anderen Weltreligion sind die verschiedenen Aspekte der Göttin so stark bewahrt worden wie dort. Diesen Aspekt besser zu verstehen, in der Theorie wie in der Praxis, ist das Thema dieses Reiseseminars. Dazu besuchen wir Ashrams (Sri Gnanananda Niketan, Sri Ramanasramam, Shantivanam, Vivekananda Kendra, Amritapuri) und Tempel (Sri Minakshi Tempel in Madurai und Kanyakumari Bhagavatiam Tempel). Der Abschluss in Amritapuri beinhaltet eine Begegnung mit der bekanntesten lebenden Heiligen Indiens, Sri Mata Amritanandamayi Devi.
+        </p>
+        <h3 class="content">
+        Reise 5: Ramakrishna Paramahamsa und Shakyamuni Buddha - 2 Archetypen indischer Spiritualität. Eine interreligiöse spirituelle Pilgerreise nach Kolkata, Shantiniketan, Bodhgaya, Benares, Kushinagar und Lumbini</h3>
+        <p class="content">
+            Diese Reise wurde konzipiert für die Evangelische Stadtakademie München. Sie orientiert sich an 2 archetypischen spirituellen Gestalten, zum einen Sri Ramakrishna Paramahamsa, der wichtigsten Gestalt der hinduistischen Renaissance des 19. Jahrhunderts, und zum anderen an Gautama Shakyamuni Buddha, dem Gründer des Buddhismus. Darüberhinaus werden wir uns aber auch mit weitergehenden kulturellen, sozialen, politischen und wirtschaftlichen Aspekte des modernen Indien befassen. (Die Reise wurde durchgeführt im Feb-März 2010, etwas modifiziert, mit Aufenthalt im Krishnamurti Study Centre Rajghat, Feb-März 2015).
+        </p>
+    </div><!--Closing tag Reisenbeschreibung-->
+    <div id="Reiseteilnahme">
+        <img src="reiseIndien2.jpg" class="imgRight" alt="Mann in einem orangen Gewand hält einen Hundewelpen in den Händen und betrachtet ihn.">
+        <h3 class="content">
+            Teilnahmevoraussetzungen:
+        </h3>
+        <ul class="content">
+            <li>Interesse an Indien und dem Hinduismus,</li>
+            <li>an Fragen des spirituellen Dialogs zwischen Christentum und Hinduismus, West und Ost, Europa und Indien.</li>
+            <li>täglich 2 x 1 Stunde persönliche spirituelle Praxis (auf Wunsch Anleitung und Begleitung)</li>
+            <li>Körperliche und psychische Gesundheit und Belastbarkeit </li>
+            <li>Bereitschaft, einfach und flexibel zu leben,</li>
+            <li>sich auf Indien und indische Spiritualität einzulassen,</li>
+            <li>sich auf die Dynamik der Gruppe einzulassen,</li>
+            <li>u.U. auch mit öffentlichen Verkehrsmitteln in Indien zu reisen,</li>
+            <li>sich mit materieller Armut zu konfrontieren</li>
+        </ul>
+        <p class="content">
+            TeilnehmerInnenzahl begrenzt auf 10-15 Personen
+        </p>
+        <h3 class="content">
+            Kosten für ca. 2-3 Wochen:
+        </h3>
+        <p class="content">
+            1800-2000,- € (inkl. Flug, Übernachtungen, Transfers, Spenden für Sozialprojekte; Flug und Visa selbst besorgen) und Honorar für den Reiseleiter: 550 € (bei kleinerer TeilnehmerInnenzahl erhöht sich letzteres).
+        </p>
+        <h3 class="content">
+            Anmeldung und Rücktrittsbedingungen:
+        </h3>
+        <p class="content">
+            Nach der Anmeldung erfolgt eine Rechnungsstellung für die Kursgebühr. Bei Rücktritt 5-3 Monate vor Seminarbeginn berechne ich eine Bearbeitungsgebühr von 50 €, danach fällt bis 1 Monat vor Beginn die halbe Kursgebühr an und ab 4 Wochen vor Seminarbeginn die gesamte Kursgebühr, es sei denn, Sie finden eine Ersatzperson. Es empfiehlt sich, die Seminargebühr in die Reiserücktrittsversicherung aufzunehmen. 
+        </p>
+    </div><!--Closing tag Reiseteilnahme-->
+    <div id="IndienSlideshow">
+        <h3 class="content">
+            Fotos von vergangenen Reisen
+        </h3>
+        <div class="slideshowContainer">
+            <figure class="slideshowFigure activeFigure">
+                <img src="2016-Binsar-1.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Binsar 2016</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2016-Corbett-1.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Corbett 2016</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2016-Haridwar-1.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Hardiwar 2016</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2016-Haridwar-2.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Hardiwar 2016</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2016-Haridwar-3.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Hardiwar 2016</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2016-Mayavati.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Mayavati 2016</figcaption>
+            </figure><figure class="slideshowFigure">
+                <img src="2016-Mayavati-HansTorwesten.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Mayavati 2016 - Hans Torwesten</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2016-Rishikesh-1.JPG" class="slideshowImg">
+                <figcaption class="anmerkung">Rishikesh 2016</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Delhi-1.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Delhi 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Orissa-1.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Orissa 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Orissa-2.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Orissa 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Orissa-3.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Orissa 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Orissa-4.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Orissa 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Orissa-5.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Orissa 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Orissa-6.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Orissa 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Orissa-7.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Orissa 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Orissa-8.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Orissa 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Orissa-9.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Orissa 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Orissa-10.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Orissa 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Rishikesh-1.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Rishikesh 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Rishikesh-2.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Rishikesh 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Simla-1.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Simla 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Simla-2.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Simla 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Varanasi-1.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Varanasi 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2017-Varanasi-2.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Varanasi 2017</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2018-Tiruvannamalai-1.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Tiruvannamalai 2018</figcaption>
+            </figure>
+            <figure class="slideshowFigure">
+                <img src="2018-Tiruvannamalai-2.jpg" class="slideshowImg">
+                <figcaption class="anmerkung">Tiruvannamalai 2018</figcaption>
+            </figure>
+            
+            <button class="PrevButton" onclick="switchImage(-1)">&#10094;</button>
+            <button class="NextButton" onclick="switchImage(1)">&#10095;</button>  
+        </div><!--Closing tag slideshowContainer-->
+        
+    </div><!--closing tag IndienSlideshow-->
+</div><!--Closing tag mainContent-->
+
+
+
+
+<?php
+require_once 'Footer.php'
+?>
+
